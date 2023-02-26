f63c454659561b29928997b7b757e21a4446f2af
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-02-26 23:23:10 +0100

    progress

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -14,31 +14,25 @@ const Wallet = (props) => {
     return (
         <InAppHBF activePage="wallet" navigation={props.navigation} headerTitleText={"Wallet"} whenHeaderMenuBtnIsPressed={() => Alert.alert("Open menu ?")} >
             <View style={[style.dashboardAssetValueDisplayRect, style.contentsInBodyCont]}>
-                <View style={[style.balanceAmountTextAndDisplayCont]}>
-                    <View style={[DefaultStyle.centeredYSpaceBetweenX]}>
-                        <Text style={[style.Balance]}>Balance</Text>
-                        <Btn text={(
-                            <FontAwesomeIcon icon="eye" color={Colors.black31} size={18} />
-                        )} onPress={() => Alert.alert("Change balance visibility ?")} />
-                    </View>
-                    <View>
-                        <Text style={[style.balanceAmount]}>$ 1,000,000.00</Text>
-                    </View>
+                <View style={[DefaultStyle.centeredX]}>
+                    <Text style={[style.Balance]}>Balance</Text>
                 </View>
-                <View style={[DefaultStyle.centeredYSpaceBetweenX, style.walletContentDecriptorCont]}>
-                    <Text style={style.walletContentDecriptorTitle} >
+                <View style={[DefaultStyle.centeredX]}>
+                    <Text style={[style.balanceAmount]}>$ 1,000,000.00</Text>
+                </View>
+            </View>
+            <View style={[style.contentsInBodyCont, style.historySectionTitle ]} >
+                <Text style={[style.historySectionTitleText]} >
+                    Assets
+                </Text>
+            </View>
+            <View>
+                <View style={[DefaultStyle.centeredYSpaceBetweenX]}>
+                    <Text>
                         Fiat
                     </Text>
-                    <Text style={style.walletContentDecriptorAmount} >
-                        900,000
-                    </Text>
-                </View>
-                <View style={[DefaultStyle.centeredYSpaceBetweenX, style.walletContentDecriptorCont]}>
-                    <Text style={style.walletContentDecriptorTitle} >
-                        Offline tokens
-                    </Text>
-                    <Text style={style.walletContentDecriptorAmount} >
-                        100,000
+                    <Text>
+                        Fiat
                     </Text>
                 </View>
             </View>
@@ -49,6 +43,72 @@ const Wallet = (props) => {
             </View>
             <View style={[style.historyContentSection, DefaultStyle.centeredXY, style.contentsInBodyCont]} >
                 {/*the content would be displayed based on state / other */}
+                <View style={[style.contentsInBodyCont, style.historyContentCont]}>
+                    <Text style={[style.historyTitle]}>
+                        Received
+                    </Text>
+                    <Text style={[style.historyText]}>
+                        $ 2,000 from @johndoe. -Offline token(s)
+                    </Text>
+                    <Text style={[style.historyDate]}>
+                        {new Date().toUTCString()}
+                    </Text>
+                </View>
+                <View style={[style.contentsInBodyCont, style.historyContentCont]}>
+                    <Text style={[style.historyTitle]}>
+                        Deposited
+                    </Text>
+                    <Text style={[style.historyText]}>
+                        $ 2,000 via crypto
+                    </Text>
+                    <Text style={[style.historyDate]}>
+                        {new Date().toUTCString()}
+                    </Text>
+                </View>
+                <View style={[style.contentsInBodyCont, style.historyContentCont]}>
+                    <Text style={[style.historyTitle]}>
+                        Withdrew
+                    </Text>
+                    <Text style={[style.historyText]}>
+                        $ 2,000 to external account.
+                    </Text>
+                    <Text style={[style.historyDate]}>
+                        {new Date().toUTCString()}
+                    </Text>
+                </View>
+                <View style={[style.contentsInBodyCont, style.historyContentCont]}>
+                    <Text style={[style.historyTitle]}>
+                        Sent
+                    </Text>
+                    <Text style={[style.historyText]}>
+                        $ 2,000 to @johndoe. -Offline token(s)
+                    </Text>
+                    <Text style={[style.historyDate]}>
+                        {new Date().toUTCString()}
+                    </Text>
+                </View>
+                <View style={[style.contentsInBodyCont, style.historyContentCont]}>
+                    <Text style={[style.historyTitle]}>
+                        Sent
+                    </Text>
+                    <Text style={[style.historyText]}>
+                        $ 2,000 to @johndoe. -Fiat
+                    </Text>
+                    <Text style={[style.historyDate]}>
+                        {new Date().toUTCString()}
+                    </Text>
+                </View>
+                <View style={[style.contentsInBodyCont, style.historyContentCont]}>
+                    <Text style={[style.historyTitle]}>
+                        Received
+                    </Text>
+                    <Text style={[style.historyText]}>
+                        $ 2,000 from @johndoe. -Fiat
+                    </Text>
+                    <Text style={[style.historyDate]}>
+                        {new Date().toUTCString()}
+                    </Text>
+                </View>
                 <Text style={{fontSize : 12, color : Colors.black46, fontFamily : "Roboto-Regular"}}>
                     No History
                 </Text>
@@ -64,14 +124,11 @@ const style = StyleSheet.create({
         maxWidth : 340,
     },
     dashboardAssetValueDisplayRect : {
-        backgroundColor : Colors.blackF2,
-        height : 204,
-        padding : 15,
+        height : 124,
+        padding : 10,
         borderRadius : 5,
-    },
-    balanceAmountTextAndDisplayCont : {
-        height : "58%",
-        justifyContent : "space-between"
+        justifyContent : "center",
+        backgroundColor : Colors.blackF2
     },
     Balance : {
         fontSize : 16,
@@ -82,22 +139,6 @@ const style = StyleSheet.create({
     balanceAmount : {
         fontSize : 30,
         color : Colors.black31,
-        fontFamily : "Roboto-Medium",
-    },
-    walletContentDecriptorCont : {
-        height : "20%",
-        marginTop : 5,
-    },
-    walletContentDecriptorTitle : {
-        fontSize : 16,
-        fontWeight : 400,
-        color : Colors.black31,
-        fontFamily : "Roboto-Medium",
-    },
-    walletContentDecriptorAmount : {
-        fontSize : 16,
-        fontWeight : 400,
-        color : Colors.black,
         fontFamily : "Roboto-Medium",
     },
     historySectionTitle : {
