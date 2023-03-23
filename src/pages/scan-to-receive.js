aa13ac186d504806130bbb5c37561cc58ced4747
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-03-23 21:22:59 +0100

    finished the basic plan for the offline transactions

--- src/pages/scan-to-receive.js
+++ src/pages/scan-to-receive.js
@@ -10,50 +10,61 @@ import * as encryptedStorage from "../functions/encrypted-storage";
 
 const ScanToReceive = (props) => {
     const storeReceivedOfOfflineTokens = async (receipt) => {
-        receipt = JSON.parse(receipt);
-        if(receipt.key == "stackion-offline-token") {
-            const userSession = await encryptedStorage.getItem("user_session");
-            if(userSession) {
-                let parsedSession = JSON.parse(userSession);
-                let offlineBalance = parsedSession.ofline_token_balance;
-                if(receipt.username == parsedSession.user_online_data.username && receipt.receiverDeviceId == parsedSession.device_id) {
-                    let offlineReceipts = [parsedSession.receipts_db];
-                    if(offlineReceipts.some(e => e.receiptId == receipt.receiptId)) {
-                        //-
+        try {
+            receipt = JSON.parse(receipt);
+            if(receipt.key == "stackion-offline-token") {
+                const userSession = await encryptedStorage.getItem("user_session");
+                if(userSession) {
+                    let parsedSession = JSON.parse(userSession);
+                    let offlineBalance = parsedSession.ofline_token_balance;
+                    if(receipt.username == parsedSession.user_online_data.username && receipt.receiverDeviceId == parsedSession.device_id) {
+                        let offlineReceipts = [parsedSession.receipts_db];
+                        if(offlineReceipts.some(e => e.receiptId == receipt.receiptId)) {
+                            Toast.show({
+                                type: 'error',
+                                text1: 'Failed attempt, can not be reused',
+                                text2: 'This receipt has been previously added to your offline balance'
+                            });
+                        }
+                        else {
+                            offlineBalance += receipt.amount;
+                            parsedSession.ofline_token_balance = offlineBalance;
+                            parsedSession.receipts_db.push(receipt);
+                            await encryptedStorage.setItem("user_session", JSON.stringify(parsedSession));
+                            Toast.show({
+                                type: 'success',
+                                text1: 'Transaction successful',
+                                text2: 'Your newly received offline tokens has been stored'
+                            });
+                            props.navigation.navigate("Dashboard");
+                        }
+                    }
+                    else if(receipt.username == parsedSession.user_online_data.username && receipt.receiverDeviceId != parsedSession.device_id) {
+                        Toast.show({
+                            type: 'error',
+                            text1: 'Opps! Wrong device',
+                            text2: 'This receipt is tied to the device the sender scanned at first'
+                        });
                     }
                     else {
                         Toast.show({
                             type: 'error',
-                            text1: 'Failed attempt, can not be reused',
-                            text2: 'This receipt has been previously added to your offline balance'
+                            text1: 'Failed attempt',
+                            text2: 'This receipt is not meant for you'
                         });
                     }
                 }
-                else if(receipt.username == parsedSession.user_online_data.username && receipt.receiverDeviceId != parsedSession.device_id) {
-                    Toast.show({
-                        type: 'error',
-                        text1: 'Opps! Wrong device',
-                        text2: 'This receipt is tied to the device the sender scanned at first'
-                    });
-                }
-                else {
-                    Toast.show({
-                        type: 'error',
-                        text1: 'Failed attempt',
-                        text2: 'This receipt is not meant for you'
-                    });
-                }
-                offlineBalance += receipt.amount;
-                parsedSession.ofline_token_balance = offlineBalance;
-                parsedSession.receipts_db.push(receipt);
+            }
+            else {
+                Toast.show({
+                    type: 'info',
+                    text1: 'Opps!',
+                    text2: 'The code you scaned is not a valid QR-receipt'
+                });
             }
         }
-        else {
-            Toast.show({
-                type: 'info',
-                text1: 'Opps!',
-                text2: 'The code you scaned is not a valid QR-receipt'
-            });
+        catch(err) {
+            //The error likely to be generated is related to trying to parse a non STRINGIFIED JSON data or a data that is not in json format.
         }
     }
     return (
